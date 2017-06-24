3d24ee35e4f782755468c4cd4b9e3d527d510c68
sixxis69 <sixxis69@gmail.com>
2017-06-24 16:09:14 +0700

    added: public folder for firebase deploy

---
+++ src/inc/firebase.php
@@ -0,0 +1,13 @@
+<script src="https://www.gstatic.com/firebasejs/4.1.3/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAOK3Up2uCE2ZFiqfGw7QtBxuKYeWV2WY4",
+    authDomain: "pwa-2017-819c0.firebaseapp.com",
+    databaseURL: "https://pwa-2017-819c0.firebaseio.com",
+    projectId: "pwa-2017-819c0",
+    storageBucket: "pwa-2017-819c0.appspot.com",
+    messagingSenderId: "290762061657"
+  };
+  firebase.initializeApp(config);
+</script>
